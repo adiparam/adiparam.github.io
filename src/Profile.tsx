a4d5a1cbe152fcf04fa846b2b503adee522a6704
Aditya Paramatmuni <github@adityaparamatmuni.com>
2025-07-08 15:43:49 -0400

    Add profile pic

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -5,8 +5,19 @@ const Profile: React.FC = () => {
   return (
     <div className="home-section">
       <div className="hero">
-        <h1>Welcome to my profile</h1>
-        <p>I'm a software engineer at <a href="https://verily.com/">Verily Life Sciences.</a></p>
+        <div className="profile-container">
+          <div className="profile-image-container">
+            <img 
+              src="/resources/profile.png" 
+              alt="Aditya Paramatmuni" 
+              className="profile-image"
+            />
+          </div>
+          <div className="profile-content">
+            <h1>Welcome to my profile</h1>
+            <p>I'm a software engineer at <a href="https://verily.com/">Verily Life Sciences.</a></p>
+          </div>
+        </div>
       </div>
     </div>
   )
