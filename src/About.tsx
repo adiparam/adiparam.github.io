b22f3f45508d898e23cb6ae7be5d253dfcf3772f
Aditya Paramatmuni <github@adityaparamatmuni.com>
2025-09-07 22:26:56 -0400

    More changes.

--- src/About.tsx
+++ src/About.tsx
@@ -13,8 +13,17 @@ const About: React.FC = () => {
                   <h3>Verily Life Sciences</h3>
                   <span className="period">2022 - Present</span>
                 </div>
-                <p>I'm currently working
-                  on <a href="https://verily.com/solutions/numetric/retinal-service">Verily Retinal Service</a>
+                <p>I architected and led the development of the primary clinical workflow on GCP using GKE microservices,
+                  enabling the launch of the 
+                  <a href="https://verily.com/solutions/numetric/retinal-service"> Verily Numetric Retinal Service </a>
+                  from concept to production. I built an 
+                  <a href="https://www.accessdata.fda.gov/scripts/cdrh/cfdocs/cfRL/rl.cfm?lid=800483&lpcd=OUG"> image
+                   storage and retrieval solution </a> called SID which has been approved by the FDA as a 
+                   Class I Software medical device system and has since served 1000s of patients.
+                </p>
+                <br/>
+                <p>I also built data processing and machine learning pipelines on GCP using Vertex AI and Cloud Run to
+                   run various analysis and image classification models on medical images at scale.
                 </p>
               </div>
               {<hr className="experience-separator" />}
@@ -25,8 +34,10 @@ const About: React.FC = () => {
                 </div>
                 <p>
                   As a tech lead for a team working on large scale data processing infrastructure, 
-                  I designed and built real-time stateful data processing systems that handled
-                  hundreds of terabytes of data daily.
+                  I designed and built large scale distributed data processing systems that are the foundation of 
+                  Google's multi-billion dollar business including YouTube and Display Ads. I worked with teams across Google to 
+                  build new storage systems that support OLAP data warehouses with 100s of Terabytes of data as well as real-time 
+                  distributed event processing infrastructure that supports 100s of billions of events per day. 
                 </p>
               </div>
               {<hr className="experience-separator" />}
@@ -35,7 +46,9 @@ const About: React.FC = () => {
                   <h3>Bank of America</h3>
                   <span className="period">2005 - 2010</span>
                 </div>
-                <p>Software Developer at Bank of America</p>
+                <p>I built financial models using Intex and Bloomberg APIs on an in-house distributed server farm 
+                  to calculate pricing and risk measures for fixed income securities.
+                </p>
               </div>
             </React.Fragment>
         </div>
@@ -50,11 +63,17 @@ const About: React.FC = () => {
                   <span className="period">May 2004</span>
                 </div>
                 <p>
-                  Various methods have been explored for building 3D models of objects. Traditionally geometric modeling techniques were used. But recently image-based modeling and rendering has been gaining a lot of popularity among researchers. <br/><br/>
-
-                  Acquisition of data is an integral process in image-based modeling. Existing approaches in acquiring realistic models are often limited by speed, cost, portability, and the objects' size, shape, and surface properties. In this thesis, we propose a novel interactive technique to address these constraints by tracking a stereo camera. Our algorithm captures a real-world object into voxels, and applies multiple statistical filtering techniqueson the resulting voxel grid in order to reduce noise that is inherent in all stereo techniques.<br/><br/>
-
-                  Resulting meshes from various models demonstrate that our algorithm is independent of object's size and shape,and works well with varying textures and surface materials.
+                  Various methods have been explored for building 3D models of objects. Traditionally geometric modeling
+                  techniques were used. But recently image-based modeling and rendering has been gaining a lot of 
+                  popularity among researchers. <br/><br/>
+                  Acquisition of data is an integral process in image-based modeling. Existing approaches in acquiring 
+                  realistic models are often limited by speed, cost, portability, and the objects' size, shape, and surface
+                  properties. In this thesis, we propose a novel interactive technique to address these constraints by 
+                  tracking a stereo camera. Our algorithm captures a real-world object into voxels, and applies multiple 
+                  statistical filtering techniqueson the resulting voxel grid in order to reduce noise that is inherent in 
+                  all stereo techniques.<br/><br/>
+                  Resulting meshes from various models demonstrate that our algorithm is independent of object's size and 
+                  shape,and works well with varying textures and surface materials.
                 </p>
               </div>
             </React.Fragment>
